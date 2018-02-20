a776261f21ebe4dab0bbe639ac478c04255c21e2
=pujilesmana <=muhammadpuji63@gmail.com>
2018-02-20 15:46:48 +0700

    Toast
    Toast untuk login

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -7,20 +7,32 @@ class Main extends CI_Controller {
 		parent::__construct();
 	}
 
-
 	public function index()
 	{
-		$this->login();
+		// $this->login();
 	}
 
-	public function login()
-	{
-		$this->load->view('login');
-	}
+	// public function login()
+	// {
+	// 	$this->load->view('login');
+	// }
 	
-	public function register()
-	{
-		$this->load->view('registrasi');
+	// public function register()
+	// {
+	// 	$this->load->view('registrasi');
+	// }
+
+	function _remap($action){
+		if($action == 1){
+			$this->loginproses();
+		}
+		else{
+			$this->load->view('login.php');
+		}
+	}
+
+	public function loginproses(){
+		echo 1;
 	}
 	
 }
